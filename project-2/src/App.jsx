fd2738077c6d31242452ba7510fe6128cea3beaa
naimatullahRezaee <naimatullahrezaee@gmail.com>
2025-01-10 10:05:50 +0430

    add form

--- project-2/src/App.jsx
+++ project-2/src/App.jsx
@@ -1,7 +1,19 @@
 function App() {
+  const eventHandler = (e) => {
+    console.log(e.target.value);
+  };
   return (
     <>
-      <div>hello</div>
+      <form action="">
+        <input type="checkbox" value="check 1" onChange={eventHandler} /> Check
+        1 <br />
+        <input type="checkbox" value="check 2" onChange={eventHandler} /> Check
+        2 <br />
+        <input type="checkbox" value="check 3" onChange={eventHandler} /> Check
+        3 <br />
+        <input type="checkbox" value="check 4" onChange={eventHandler} /> Check
+        4 <br />
+      </form>
     </>
   );
 }
